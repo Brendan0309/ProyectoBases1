ca6d8e7e408ecb772c37047fc9925caa7f66783d
Kurotito <arramirezm05@gmail.com>
2025-10-12 23:44:33 -0600

    Inserts (Country, province, canton, district)
    En district me enfoqué en los de San José.

--- Project1.sql
+++ Project1.sql
@@ -125,7 +125,7 @@ BEGIN
 END beforeUpdateProvince;
 /
 
---Canton
+--Canton tables
 CREATE TABLE canton (
     id_canton NUMBER PRIMARY KEY,
     name VARCHAR2(50),
@@ -924,25 +924,180 @@ CREATE SEQUENCE seq_collectioncenterXunit
     NOCACHE 
     NOCYCLE;
 
+--Fixed Tables Inserts
+--Parameter Inserts
+--Log Inserts
+--Country Inserts
+INSERT INTO country (seq_country.NEXTVAL, name) VALUES (seq_country.NEXTVAL, 'Costa Rica');
+--Province Inserts
+INSERT INTO province (id_province, name, id_country) VALUES (seq_province.NEXTVAL, 'San Jose', 0);
+INSERT INTO province (id_province, name, id_country) VALUES (seq_province.NEXTVAL, 'Alajuela', 0);
+INSERT INTO province (id_province, name, id_country) VALUES (seq_province.NEXTVAL, 'Cartago', 0);
+INSERT INTO province (id_province, name, id_country) VALUES (seq_province.NEXTVAL, 'Heredia', 0);
+INSERT INTO province (id_province, name, id_country) VALUES (seq_province.NEXTVAL, 'Guanacaste', 0);
+INSERT INTO province (id_province, name, id_country) VALUES (seq_province.NEXTVAL, 'Puntarenas', 0);
+INSERT INTO province (id_province, name, id_country) VALUES (seq_province.NEXTVAL, 'Limon', 0);
+-- Canton Inserts
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'San Jose', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Escazu', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Desamparados', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Puriscal', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Tarrazú', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Aserrí', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Mora', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Goicoechea', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Santa Ana', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Alajuelita', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Vásquez de Coronado', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Acosta', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Tibás', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Moravia', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Montes de Oca', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Turrumbares', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Dota', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Curridabat', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Perez Zeledon', 0);
+INSERT INTO canton (id_canton, name, id_province) VALUES (seq_canton.NEXTVAL, 'Leon Cortez Castro', 0);
 
+--District Inserts
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Carmen', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Merced', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Hospital', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Catedral', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Zapote', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Francisco de Dos Rios', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Uruca', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Mata Redonda', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Pavas', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Hatillo', 0);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Sebastian', 0);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Escazú', 1);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Antonio', 1);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Rafael', 1);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Desamparados', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Miguel', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Juan de Dios', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Rafael Arriba', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Antonio', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Frailes', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Patarrá', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Cristóbal', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Rosario', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Damas', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Rafel Abajo', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Gravilas', 2);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Los Guido', 2);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Santiago', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Mercedes Sur', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Barbacoas', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Grifo Alto', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Rafael', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Candelarita', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Desamparaditos', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Antonio', 3);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Chires', 3);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Marcos', 4);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Lorenzo', 4);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Carlos', 4);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Aserrí', 5);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Tarbaca', 5);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Vuelta de Jorco', 5);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Gabriel', 5);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Legua', 5);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Monterrey', 5);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Salitrillos', 5);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Colón', 6);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Guayabo', 6);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Tabarcia', 6);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Piedras Negras', 6);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Picagres', 6);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Jaris', 6);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Quitirrisí', 6);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Guadalupe', 7);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Francisco', 7);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Calle Blancos', 7);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Mata de Plátano', 7);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Ipís', 7);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Rancho Redondo', 7);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Purral', 7);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Santa Ana', 8);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Salitral', 8);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Pozos', 8);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Uruca', 8);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Piedades', 8);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Brasil', 8);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Alajuelita', 9);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Josecito', 9);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Antonio', 9);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Alajuelita', 9);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Concepción', 9);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Falipe', 9);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Isidro', 10);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Rafael', 10);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Dulce de Nombre de Jesús', 10);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Patalillo', 10);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Cascajal', 10);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Ignacio', 11);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Guaitil Villa', 11);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Palmichal', 11);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Cangrejal', 11);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Sabanillas', 11);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Juan', 12);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Cinco Esquinas', 12);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Anselmo Llorente', 12);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'León XIII', 12);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Colima', 12);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Vicente', 13);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Jerónimo', 13);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'La Trinidad', 13);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Pedro', 14);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Sabanilla', 14);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Mercedes', 14);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Rafael', 14);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Pablo', 15);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Pedro', 15);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Juan de Mata', 15);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Luis', 15);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Carara', 15);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Santa María', 16);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Jardín', 16);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Copey', 16);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Curridabat', 17);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Granadilla', 17);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Sánchez', 17);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Tirrases', 17);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Isidro de el General', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'El General', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Daniel Flores', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Rivas', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Pedro', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Platanares', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Pejibaye', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Cajón', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Barú', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Río Nuevo', 18);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Páramo', 18);
 
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Isidro', 19);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'Santa Cruz', 19);
+INSERT INTO district (id_district, name, id_canton) VALUES (seq_district.NEXTVAL, 'San Antonio', 19);
 
-
+--Entity
